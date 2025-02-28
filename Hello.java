16a780727788c07fb9baf2aa35cab92209a0fb7c
AKAEnon <enondemori@gmail.com>
2025-02-27 21:44:52 -0300

    portugues

--- Hello.java
+++ Hello.java
@@ -3,8 +3,12 @@ package aula2;
 public class Hello {
 
 	public static void main(String[] args) {
-	System.out.println("Hello World");
-
+	port();
 	}
-
+	 public static void port() {
+	System.out.println("Olá Mundo");
+	}
+	 public static void eng() {
+	System.out.println("Hello World");
+	}
 }
