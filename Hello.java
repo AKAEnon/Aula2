6d056753c506ecad6f03c7c2414de3c339d4f83e
AKAEnon <enondemori@gmail.com>
2025-02-27 21:30:53 -0300

    port

--- Hello.java
+++ Hello.java
@@ -3,8 +3,13 @@ package aula2;
 public class Hello {
 
 	public static void main(String[] args) {
-	System.out.println("Hello World");
 
+		port();
 	}
-
+	public static void eng() {
+		System.out.println("Hello World");
+	}
+	public static void port() {
+		System.out.println("Olá Mundo");
+	}
 }
